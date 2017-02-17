cf0534dba2652ec7650d6d93583e863c3f1984ad
hojnacki <hojnacki@lab1-22.eng.utah.edu>
2017-02-17 13:05:52 -0700

    Fixed inventory map bug where wasn't removing expired inventory items, updated output.

--- assignment4/processor/warehouse.cpp
+++ assignment4/processor/warehouse.cpp
@@ -13,13 +13,11 @@ using namespace std;
 warehouse::warehouse(string name) :
   name(name)
 {
-  cout << "Called named constructor ------------------" << endl;
 }
 
 warehouse::warehouse () :
   name("")
 {
-  cout << "Called defult constructor ----" << endl;
 }
 
 string warehouse::get_name()
@@ -30,7 +28,6 @@ string warehouse::get_name()
 vector<string> warehouse::get_upc_codes() const 
 {
   vector<string> upcCodes;
-  cout << "iM size " << inventoryMap.size() << endl;
   for (auto kv : inventoryMap)
   {
     upcCodes.push_back(kv.first);
@@ -40,7 +37,6 @@ vector<string> warehouse::get_upc_codes() const
 
 void warehouse::process_transaction(transaction trans, food_item foodItem)
 {
-  cout << "Transac Process" << endl;
   if (trans.get_type() == "request")
   {
     request(foodItem, trans.get_count());
@@ -53,32 +49,25 @@ void warehouse::process_transaction(transaction trans, food_item foodItem)
 
 void warehouse::request(food_item foodItem, int count) 
 {
-  cout << "Request begin" << endl;
   int amountLeft = count;
   if (inventoryMap.find(foodItem.get_upc()) != inventoryMap.end())
   {
-    cout << "a" << endl;
     // Add item to queue
     queue<inventory_item> & candidates = inventoryMap[foodItem.get_upc()];
     while (amountLeft > 0)
     {
-      cout << "Amount Left: " << amountLeft << endl;
-      
       inventory_item & currItem = candidates.front();
       if (currItem.get_quantity() > amountLeft) 
       {
-        cout << "b" << endl;
         currItem.decrement_quantity(amountLeft);
         amountLeft = 0;
       }
       else
       {
-        cout << "c" << endl;
         // We have to totally get rid of this inventory item
         amountLeft -= currItem.get_quantity();
-        cout << "d" << endl;
         candidates.pop();
-        cout << "e" << endl;
+
         // Now erase from expiration date
         expirationDateMap["" + currItem.get_expiration_date()].erase(currItem.get_upc());
       }
@@ -90,8 +79,6 @@ void warehouse::request(food_item foodItem, int count)
     }
     
   }
-  cout << "End amount left " << amountLeft << endl;
-  // Warehouse does not contain item so ignore request
 }
 
 /*
@@ -109,6 +96,7 @@ void warehouse::receive(food_item foodItem, int count, int currDate)
   {
     
     queue<inventory_item> & items = inventoryMap[foodItem.get_upc()];
+    cout << "Size of queue: " <<  items.size() << endl;
     if (items.back().get_expiration_date() == item.get_expiration_date())
     {
       // Then we can just increment quantity and be done
@@ -120,35 +108,37 @@ void warehouse::receive(food_item foodItem, int count, int currDate)
       // This needs to be pushed on the queue
       items.push(item);
     }
+    cout << "Size of queue: " << items.size() << endl;
   }
-  else // WE don have this item in inventory
+  else // We dont have this item in inventory
   {
     cout << "item not in inventory yet" << endl;
     // Create new queue and add to inventory map
     queue<inventory_item> inventoryItems;
     cout << "new que created" << endl;
     inventoryItems.push(item);
-    cout << "pusged item" << endl;
+    cout << "pushed item onto queue" << endl;
     inventoryMap.emplace(foodItem.get_upc(), inventoryItems);
-    cout << "emplaced" << endl;
+    cout << "add queue with fooditem upc to inventory map." << endl;
   }
 
   cout << item.get_expiration_date() << endl;
   // Add to expiration date map  
   if (expirationDateMap.find("" + item.get_expiration_date()) != expirationDateMap.end())
   {
-    cout << "e" << endl;
+    cout << "Adding item to expiration date map" << endl;
     // Add item to map
     expirationDateMap["" + item.get_expiration_date()].emplace(item.get_upc(), item);
   }
   else 
   {
-    cout << "f" << endl;
     // Create new map and add item, then add to expiration map
     unordered_map<string, inventory_item> inventoryItems;
     inventoryItems.emplace(item.get_upc(), item);
+    cout << "expiredMap size : " << expirationDateMap.size() << endl;
     expirationDateMap.emplace("" + item.get_expiration_date(), inventoryItems);
-  }
+    cout << "expiredMap size: " << expirationDateMap.size() << endl;
+  }    
 }
 
 /*
@@ -156,26 +146,30 @@ void warehouse::receive(food_item foodItem, int count, int currDate)
  */
 void warehouse::remove_at_expiration_date(const int expirationDate) 
 {
-  cout << "Expiration " << expirationDate << endl;
+  cout << "Expiration Date: " << expirationDate << endl;
   if (expirationDateMap.find("" + expirationDate) != expirationDateMap.end()) // There is expired items
   {
-    cout << "deleting" << endl;
-    // TODO might be issue with key for expiredItemsMap not matching its values expiraation date
+    cout << "Iterating through expired Items Map.." << endl;
+    // TODO might be issue with key for expiredItemsMap not matching its values expiration date
     unordered_map<string, inventory_item> expiredItemsMap = expirationDateMap["" + expirationDate];
     for (auto kv: expiredItemsMap) 
     {    
       inventory_item &  expiredItem = kv.second;
-      cout << expiredItem.get_upc() << endl;
-      cout << "eI date " << expiredItem.get_expiration_date() << endl;
       // delete this from inventory map
       queue<inventory_item> & candidates = inventoryMap[expiredItem.get_upc()];
-      cout << "cand size " << candidates.size() << endl;
+      cout << "queue size: " << candidates.size() << endl;
       // Remove all candidates with expired date
-      cout << "cF date " << candidates.front().get_expiration_date() << endl;
       while (candidates.front().get_expiration_date() == expirationDate)
       {
         cout << "popping" << endl;
         candidates.pop();
+	cout << "queue size: " << candidates.size() << endl;
+	cout << "inventoryMap size: " << inventoryMap.size() << endl;
+      }
+      // If all inventory items with upc code are removed/expired. Delete that  entry in inventory map.
+      if (candidates.size() == 0)
+      {
+	inventoryMap.erase(expiredItem.get_upc());
       }
     }
     // Erase expired items
